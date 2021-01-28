000e19c36e20dac6b88dda257e58d662b2047d90
akhilmeena <48666821+akhilmeena@users.noreply.github.com>
2021-01-29 00:01:18 +0530

    update couple2.py

--- couple2.py
+++ couple2.py
@@ -119,10 +119,11 @@ def command_default(m):
   #bot.send_photo(chat_id=[ "-1001264715334"], photo=photo_id, caption="💄👙❣️👌👈💦\n\n" + f"{Pk1} {Pk2} {Pk3}",reply_markup=keyboard)
   except Exception as e:
     bot.send_message(m.chat.id,e)
-  
-  
-  
-  
+
+
+
+
+
 @bot.message_handler(func=lambda message:True, content_types=['video'])
 def command_default(m):
   Akh =["#Hot","#Cool","#Brave"]
