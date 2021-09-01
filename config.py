d5f05f0da722b2da01ba9ec21c9acf9b81224f09
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-09-01 21:13:42 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 BOT_TOEN = "1902307802:AAG0D1WZSDVCzWWsMzwSAXJq_1-O9MDsNA4"
 chnlid = "1551862526"
-CHANNEL_ID="-100" + int(ak)
+CHANNEL_ID="-100" + int(chnlid)
 SHEETA = "data1"
-app = "pdiskdevu123"
+app = "akhilpdisk"
