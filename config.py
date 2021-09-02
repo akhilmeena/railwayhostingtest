89ea34f266496deeca6fab7ffb95d68ef88c48b7
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-09-02 09:01:42 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 class Config:
-    BOT_TOEN = "1902307802:AAG0D1WZSDVCzWWsMzwSAXJq_1-O9MDsNA4"
+    BOT_TOKEN = "1902307802:AAG0D1WZSDVCzWWsMzwSAXJq_1-O9MDsNA4"
     SHEETA = "data1"
     app = "akhilpdisk"
     chnlid = "1551862526"
