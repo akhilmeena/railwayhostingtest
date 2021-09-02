51aef1a9ce35e26680aa8b581fe14f40a41e4240
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-09-02 08:35:06 +0530

    update couple2.py

--- couple2.py
+++ couple2.py
@@ -126,7 +126,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://akhilpdisk.herokuapp.com/' + config.TOKEN)
+    bot.set_webhook(url='https://' + config.app + '.herokuapp.com/' + config.TOKEN)
     return "!", 200
  
 if __name__ == "__main__":
