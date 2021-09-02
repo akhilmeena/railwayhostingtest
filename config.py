06438a29d2688d17426d76a6ad029338c96393ed
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-09-02 08:47:37 +0530

    update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
-BOT_TOEN = "1902307802:AAG0D1WZSDVCzWWsMzwSAXJq_1-O9MDsNA4"
-SHEETA = "data1"
-app = "akhilpdisk"
-chnlid = "1551862526"
-CHANNEL_ID="-10" + chnlid
+import os
+class Config:
+  BOT_TOEN = "1902307802:AAG0D1WZSDVCzWWsMzwSAXJq_1-O9MDsNA4"
+  SHEETA = "data1"
+  app = "akhilpdisk"
+  chnlid = "1551862526"
+  CHANNEL_ID="-10" + chnlid
