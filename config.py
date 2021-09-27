fb401d792e41d6a3129bafd5749216d29b132d33
AKHILESH KUMAR <48666821+akhilmeena@users.noreply.github.com>
2021-09-27 17:13:17 +0530

    update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 class Config:
     BOT_TOKEN = "1902307802:AAG0D1WZSDVCzWWsMzwSAXJq_1-O9MDsNA4"
     SHEETA = "data1"
-    app = "akhilpdisk"
+    app = "akhilpdisk1"
     chnlid = "1551862526"
     CHANNEL_ID="-100" + chnlid
